a724b2197f7afa709fad3c0f7cd453091ce657b5
Etherian <etherain@gmail.com>
2021-05-11 14:41:55 -0400

    add outline for app

--- src/App.java
+++ src/App.java
@@ -1,9 +1,33 @@
 
 public class App {
 
+	enum Pizza_Size {
+	};
+	
+	enum Drink_Size {
+	};	
+	
+	record Order (
+	) {}
+	
+	// TODO Add the rest of the necessary data structures
+	// TODO Fill in data structures
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Print greeting
+		
+		// TODO Ask for name
+		
+		// TODO Print menu and current order
+		
+		// TODO Take order
+		// 1. Gather input
+		// 2. Change order based on input
+		// 3. Print new order
+		// 4. Return to step 1 or finish order
+		
+		// TODO Calculate and print summary
+		
 	}
 
 }
