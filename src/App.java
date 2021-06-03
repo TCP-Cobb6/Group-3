0eb7871daece9bd64f47a78f7a4edd19695b07b4
courtneyfrancis32 <courtneyfrancis32@gmail.com>
2021-06-02 20:27:35 -0400

    Cut and pasted into appjava

--- src/App.java
+++ src/App.java
@@ -30,4 +30,94 @@ public class App {
 		
 	}
 
+		String[] sizes = {"Personal", "Medium", "Large"};
+			
+			double Personal = 9.99;
+			double Medium = 12.99;
+			double Large =  14.99;
+			System.out.println("My selection is");
+			
+			String Toppings = "Meats" + "Veggies";
+	        
+		
+			int Meats = 0;
+			switch (Meats)  {
+			
+			case 1:
+				System.out.print("Pepperoni " +"0.50");
+				break;
+				
+			case 2:
+				System.out.print("Bacon " + 0.75);
+				break;
+			
+				
+			case 3:
+				System.out.print("Philly Cheesesteak " + 1.50);
+				break;
+				
+				
+			case 4:
+				System.out.print("Ground beef " + 1.00);
+				break;
+				
+			case 5:
+				System.out.print("Chicken " + 1.00);
+				
+				
+				
+				int Veggie = 0;
+			switch (Veggie) {
+			
+			case 1:
+				System.out.print("Extra Cheese " + 0.50);
+				break;
+			
+			case 2:
+		        System.out.print("Jalapenos " + 0.75);
+				break;
+			
+			case 3:
+				System.out.print("Green Peppers " + 0.75);
+				break;
+			
+			case 4:
+				System.out.print("Artichoke " + 1.25);
+				break;
+			
+			case 5:
+				System.out.print("Olives " + 1.00);
+				break;
+			
+			case 6:
+				System.out.print("Onions " + 0.75);
+				break;
+				
+				
+				int Drinks = 0;
+				switch (Drinks)  {
+				
+				case 1:
+					System.out.println("Medium " + 1.99);
+					break;
+				
+				case 2:
+					System.out.print("Large " + 2.99);
+					break;
+				
+				case 3:
+					System.out.print("Family 2 liter " + 4.99);
+					default:
+						  
+			}
+			
+			}
+	
+			}	
+		}
+	}
+	
+	
+	
+
 }
