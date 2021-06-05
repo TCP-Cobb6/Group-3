636145e49d91e46b3c7118b1dae839f749883518
Etherian <etherain@gmail.com>
2021-06-05 14:32:11 -0400

    added placeholder types and values

--- src/App.java
+++ src/App.java
@@ -1,22 +1,55 @@
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class App {
 
 	enum Pizza_Size {
+		Personal,
+		Medium,
+		Large
 	};
 	
 	enum Drink_Size {
-	};	
+		Medium,
+		Large,
+		Family
+	};
+	
+	enum Toppings {
+		Pepperoni,
+		Ground_beef,
+		Bacon,
+		Chicken,
+		Philly_Cheesesteak,
+		Extra_Cheese,
+		Jalapenos,
+		Green_peppers,
+		Artichoke,
+		Olives,
+		Onions
+	}
+	
+	record Pizza (
+		Pizza_Size Size,
+		HashSet<Toppings> Toppings
+	) {}
 	
 	record Order (
+			boolean Delivery,
+			Pizza Pizza,
+			Drink_Size Drink
 	) {}
 	
 	// TODO Add the rest of the necessary data structures
 	// TODO Fill in data structures
 	
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		// TODO Print greeting
 		
 		// TODO Ask for name
+        System.out.print("What is your name? ");
+        String user = in.next();
 		
 		// TODO Print menu and current order
 		
