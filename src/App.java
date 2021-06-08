8f05e1c8cba9d786afa86005fd56fa71235dba2c
courtneyfrancis32 <courtneyfrancis32@gmail.com>
2021-06-08 14:01:25 -0400

    Reconfigured code, and it works now

--- src/App.java
+++ src/App.java
@@ -1,123 +1,70 @@
 
-public class App {
+import java.util.HashMap;
+import java.util.HashSet;
 
-	enum Pizza_Size {
+public class Pizza_Machine {
+
+
+enum Pizza_Size{ Personal, Medium, Large};
+
+
+enum Toppings_Meats{
+Pepperoni,
+Groundbeef,
+Bacon,Chicken,
+PhillyCheeseSteak};	
+
+
+enum Toppings_Veggie {
+ExtraCheese,
+Jalapenos,
+GreenPeppers,
+Artichoke,
+Olives,
+Onions};
+
+enum Drink_Size { Medium, Large, Family
 	};
-	
-	enum Drink_Size {
-	};	
-	
-	record Order (
-	) {}
-	
-	// TODO Add the rest of the necessary data structures
-	// TODO Fill in data structures
-	
-	public static void main(String[] args) {
-		// TODO Print greeting
-		
-		// TODO Ask for name
-		
-		// TODO Print menu and current order
-		
-		// TODO Take order
-		// 1. Gather input
-		// 2. Change order based on input
-		// 3. Print new order
-		// 4. Return to step 1 or finish order
-		
-		// TODO Calculate and print summary
-		
-	}
+	enum Drink_Type {Grape, Orange};
 
-		String[] sizes = {"Personal", "Medium", "Large"};
-			
-			double Personal = 9.99;
-			double Medium = 12.99;
-			double Large =  14.99;
-			System.out.println("My selection is");
-			
-			String Toppings = "Meats" + "Veggies";
-	        
-		
-			int Meats = 0;
-			switch (Meats)  {
-			
-			case 1:
-				System.out.print("Pepperoni " +"0.50");
-				break;
-				
-			case 2:
-				System.out.print("Bacon " + 0.75);
-				break;
-			
-				
-			case 3:
-				System.out.print("Philly Cheesesteak " + 1.50);
-				break;
-				
-				
-			case 4:
-				System.out.print("Ground beef " + 1.00);
-				break;
-				
-			case 5:
-				System.out.print("Chicken " + 1.00);
-				
-				
-				
-				int Veggie = 0;
-			switch (Veggie) {
-			
-			case 1:
-				System.out.print("Extra Cheese " + 0.50);
-				break;
-			
-			case 2:
-		        System.out.print("Jalapenos " + 0.75);
-				break;
-			
-			case 3:
-				System.out.print("Green Peppers " + 0.75);
-				break;
-			
-			case 4:
-				System.out.print("Artichoke " + 1.25);
-				break;
-			
-			case 5:
-				System.out.print("Olives " + 1.00);
-				break;
-			
-			case 6:
-				System.out.print("Onions " + 0.75);
-				break;
-				
-				
-				int Drinks = 0;
-				switch (Drinks)  {
-				
-				case 1:
-					System.out.println("Medium " + 1.99);
-					break;
-				
-				case 2:
-					System.out.print("Large " + 2.99);
-					break;
-				
-				case 3:
-					System.out.print("Family 2 liter " + 4.99);
-					default:
-						  
-			}
-			
-			}
+	record Drink(Drink_Size Size, Drink_Type Flavor) {}
 	
-			}	
-		}
+	record Order(boolean Dellivery, String Pizza, String Drink) {}
+	record pizza (Pizza_Size Size,HashSet<Toppings_Meats> ToppingsOrder,HashSet <Toppings_Veggie>VeggieToppings) {}
+	record drink (Drink_Size Size,HashSet<Drink_Size>  Drinks) {}
+	public static void main(String[] args) 
+	{
+		HashMap<Pizza_Size, Double> pizza_size_prices = new HashMap<>();
+pizza_size_prices.put(Pizza_Size.Personal, 9.99);
+pizza_size_prices.put(Pizza_Size.Medium, 12.99);
+pizza_size_prices.put(Pizza_Size.Large, 14.99);
+
+HashMap<Drink_Size,Double> DrinksPrice = new HashMap<>();
+DrinksPrice.put(Drink_Size.Medium,(1.99));
+DrinksPrice.put(Drink_Size.Large,(2.99));
+DrinksPrice.put(Drink_Size.Family,(4.99));
+
+
+
+
+
+HashMap<Toppings_Meats, Double> MeatToppingPrice = new HashMap<>();
+MeatToppingPrice.put(Toppings_Meats.Pepperoni, (0.50));
+MeatToppingPrice.put(Toppings_Meats.Groundbeef, (1.00));
+MeatToppingPrice.put(Toppings_Meats.Bacon, (0.75));
+MeatToppingPrice.put(Toppings_Meats.Chicken, (1.00));
+MeatToppingPrice.put(Toppings_Meats.PhillyCheeseSteak, (1.50));
+
+HashMap<Toppings_Veggie, Double> VeggieToppingPrice = new HashMap<>();
+VeggieToppingPrice.put(Toppings_Veggie.ExtraCheese, (0.50));
+VeggieToppingPrice.put(Toppings_Veggie.Jalapenos, (0.50));
+VeggieToppingPrice.put(Toppings_Veggie.GreenPeppers, (0.75));
+VeggieToppingPrice.put(Toppings_Veggie.Artichoke, (1.20));
+VeggieToppingPrice.put(Toppings_Veggie.Olives, (1.00));
+VeggieToppingPrice.put(Toppings_Veggie.Onions, (0.75));
+
+
+
 	}
-	
-	
-	
 
 }
